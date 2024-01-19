e776b0a3ca371d7bfddcc68b7c1538d5b5be6371
Eduardo Barroso <git@edbar.xyz>
2024-01-19 15:24:07 -0300

    cleanup:  Split window class and main file; adds gitignore

---
+++ window.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, BOTH, Canvas
+
+class  Window():
+    def __init__(self, width, height):
+        self.__root =  Tk()
+        self.__root.title =  "Maze Solver"
+        self.__root.protocol("WM_DELETE_WINDOW",  self.close)
+        self.__canvas =  Canvas()
+        self.__canvas.pack()
+        self.__window_running  = False 
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__window_running =  True
+        while(self.__window_running): 
+            self.redraw()
+
+    def close(self):
+        self.__window_running = False
