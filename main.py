6ed9263a3ed73d49bbebe1f45e0c028c5f5fdcf7
Eduardo Barroso <git@edbar.xyz>
2024-01-19 15:14:55 -0300

    first commit: open window  with blank canvas

---
+++ main.py
@@ -0,0 +1,32 @@
+from tkinter import Tk, BOTH, Canvas
+
+class  Window():
+    def __init__(self, width, height):
+        self.__root =  Tk()
+        self.__root.title =  "Maze Solver"
+        self.__root.protocol("WM_DELETE_WINDOW",  self.close)
+        self.__canvas =  Canvas()
+        self.__canvas.pack()
+        self.__window_running  = False 
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__window_running =  True
+        while(self.__window_running): 
+            self.redraw()
+
+    def close(self):
+        self.__window_running = False
+
+
+def main():
+   win  = Window(800, 600)
+   win.wait_for_close() 
+
+if __name__  ==  "__main__":
+    main()
+
+
